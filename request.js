9092e5af50b0717ba924d2bb7dc01b30e6ffe595
aa6 <justsunshineandbluesky@gmail.com>
2016-06-23 11:16:03 +0300

    Request support.

---
+++ request.js
@@ -0,0 +1,14 @@
+var ppromisify = require('./ppromisify.js')
+
+module.exports = ppromisify(
+    require('request'),
+    {
+        ''      : ppromisify.callback_err_res,
+        'del'   : ppromisify.callback_err_res,
+        'get'   : ppromisify.callback_err_res,
+        'put'   : ppromisify.callback_err_res,
+        'head'  : ppromisify.callback_err_res,
+        'post'  : ppromisify.callback_err_res,
+        'patch' : ppromisify.callback_err_res,
+    }
+)
