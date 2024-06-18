1bd787686a7412a15a8bc2ab331c4a75d55abc0a
aa6 <justsunshineandbluesky@gmail.com>
2024-06-18 20:03:24 +0400

    Fix: missing functions.

--- request.js
+++ request.js
@@ -4,11 +4,13 @@ module.exports = ppromisify(
     require('request'),
     {
         '<call>' : ppromisify.callback_err_res,
+        'jar'    : ppromisify.function_as_is,
         'del'    : ppromisify.callback_err_res,
         'get'    : ppromisify.callback_err_res,
         'put'    : ppromisify.callback_err_res,
         'head'   : ppromisify.callback_err_res,
         'post'   : ppromisify.callback_err_res,
         'patch'  : ppromisify.callback_err_res,
+        'cookie' : ppromisify.function_as_is,
     }
 )
